0f27e33dbf29b878b0b51e16c0fbd39a7bc329ef
logcet <111567875+logcet@users.noreply.github.com>
2023-01-07 20:50:14 +0200

    Add files via upload

---
+++ script .js
@@ -0,0 +1,11 @@
+
+
+
+function hed() {
+    document.getElementById("img").style.display = "none";
+  document.getElementById("tex").style.display= "block";
+}
+function opp() {
+    document.getElementById("img").style.display = "block";
+  document.getElementById("tex").style.display= "none";
+}
